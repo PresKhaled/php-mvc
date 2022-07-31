f477e4abbf6c350126a0469a686fac1f0cc451e8
Khaled Mohsen <pres.kbayomy@gmail.com>
2022-07-31 18:31:10 +0200

    Run the application.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,7 @@
 <?php
 
-use Khaled\PhpMvc\http\Connection;
-use Khaled\PhpMvc\http\Response;
-use Khaled\PhpMvc\http\Route;
+require_once 'app/helpers.php';
+require_once base_path() . '/vendor/autoload.php';
+require_once base_path() . '/routes/web.php';
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../routes/web.php';
-
-$route = new Route(new Connection, new Response);
-$route->resolve();
+app()->run();
