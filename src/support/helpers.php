4b904b9349de7b06e8ba793c15a224f50976dfda
Khaled Mohsen <pres.kbayomy@gmail.com>
2022-08-08 22:36:56 +0200

    Add more helper functions.

--- src/support/helpers.php
+++ src/support/helpers.php
@@ -3,12 +3,23 @@
 use App\Application;
 use App\Config;
 use App\View;
-use App\WithArrayAccess;
 use JetBrains\PhpStorm\NoReturn;
+use JetBrains\PhpStorm\Pure;
 use Khaled\PhpMvc\http\Connection;
+use Khaled\PhpMvc\support\Hash;
+use Khaled\PhpMvc\support\WithArrayAccess;
 use Minwork\Helper\Arr;
 
+const FLASH_MESSAGES_KEY = 'flash_messages';
+const OLD_VALUES_KEY = 'old_values';
+const OLD_VALUES_NEVER_FLUSH = ['password', 'password_confirmation'];
+
 if (!function_exists('app')) {
+    /**
+     * -
+     *
+     * @return Application|null
+     */
     function app(): ?Application
     {
         static $app = null;
@@ -22,21 +33,36 @@ if (!function_exists('app')) {
 }
 
 if (!function_exists('base_path')) {
+    /**
+     * -
+     *
+     * @return string
+     */
     function base_path(): string
     {
-        return __DIR__ . '/../';
+        return __DIR__ . '/mvc/';
     }
 }
 
 if (!function_exists('views_path')) {
-    function views_path(): string
+    /**
+     * -
+     *
+     * @return string
+     */
+    #[Pure] function views_path(): string
     {
         return base_path() . '/views';
     }
 }
 
 if (!function_exists('parts_path')) {
-    function parts_path(): string
+    /**
+     * -
+     *
+     * @return string
+     */
+    #[Pure] function parts_path(): string
     {
         return base_path() . '/views/layouts/parts';
     }
@@ -44,6 +70,8 @@ if (!function_exists('parts_path')) {
 
 if (!function_exists('view')) {
     /**
+     * -
+     *
      * @throws Exception
      */
     #[NoReturn] function view(string $path, array $params = []): void
@@ -54,7 +82,12 @@ if (!function_exists('view')) {
 }
 
 if (!function_exists('config_path')) {
-    function config_path(): string
+    /**
+     * -
+     *
+     * @return string
+     */
+    #[Pure] function config_path(): string
     {
         return base_path() . '/config';
     }
@@ -67,7 +100,8 @@ if (!function_exists('config')) {
      * @return mixed|array
      * @throws Exception
      */
-    function config(string $path): mixed {
+    function config(string $path): mixed
+    {
         $parts = explode('.', $path);
         $fileBasename = $parts[0] ??= $path; // Specified key(s) or not.
         $filePath = (config_path() . '/' . $fileBasename . '.php');
@@ -94,24 +128,44 @@ if (!function_exists('config')) {
 }
 
 if (!function_exists('app_str_class_format')) {
-    function app_str_class_format(string $value): string {
-        $value      = (explode(':', $value)[0]);
-        $separator  = '_';
+    /**
+     * -
+     *
+     * @param string $value
+     * @return string
+     */
+    function app_str_class_format(string $value): string
+    {
+        $value = (explode(':', $value)[0]);
+        $separator = '_';
 
         return str_replace($separator, '', ucwords($value, $separator));
     }
 }
 
-/*if (!function_exists('when')) {
-    function when(mixed $value, mixed $conditions): bool {
-        if ($conditions) {
-            return $value;
-        }
+if (!function_exists('bcrypt')) {
+    /**
+     * -
+     *
+     * @param string $password
+     * @return string
+     */
+    function bcrypt(string $password): string
+    {
+        return Hash::password($password);
     }
-}*/
+}
 
 if (!function_exists('env')) {
-    function env(string $key, mixed $default = ''): mixed {
+    /**
+     * -
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    function env(string $key, mixed $default = ''): mixed
+    {
         return ($_ENV[$key] ??= $default);
     }
 }
@@ -121,7 +175,7 @@ if (!function_exists('with_array_access')) {
      * -
      *
      * @param array $results
-     * @return WithArrayAccess
+     * @return \Khaled\PhpMvc\support\WithArrayAccess
      */
     function with_array_access(array $results): WithArrayAccess
     {
@@ -137,6 +191,11 @@ if (!function_exists('with_array_access')) {
 }
 
 if (!function_exists('connection')) {
+    /**
+     * -
+     *
+     * @return Connection
+     */
     function connection(): Connection
     {
         return app()->connection;
@@ -144,6 +203,11 @@ if (!function_exists('connection')) {
 }
 
 if (!function_exists('back')) {
+    /**
+     * -
+     *
+     * @return void
+     */
     #[NoReturn] function back(): void
     {
         header('Location: ' . $_SERVER['HTTP_REFERER']);
@@ -152,10 +216,18 @@ if (!function_exists('back')) {
 }
 
 if (!function_exists('old')) {
+    /**
+     * -
+     *
+     * @param string $name
+     * @return string
+     */
     function old(string $name): string
     {
-        if (app()->session->has($name, true, true)) {
-            return app()->session->get($name, true, true);
+        $keys = (FLASH_MESSAGES_KEY . '.' . OLD_VALUES_KEY . '.' . $name);
+
+        if (app()->session->has($keys)) {
+            return (app()->session->getFlash($name, OLD_VALUES_KEY));
         }
 
         return '';
