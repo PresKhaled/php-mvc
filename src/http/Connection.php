94100ebde2569e88a27eaeb75c084d38dc65b00d
Khaled Mohsen <pres.kbayomy@gmail.com>
2022-08-08 22:41:19 +0200

    Improve [Connection] class.

--- src/http/Connection.php
+++ src/http/Connection.php
@@ -2,23 +2,46 @@
 
 namespace Khaled\PhpMvc\http;
 
+use JetBrains\PhpStorm\Pure;
+
 class Connection
 {
+    /**
+     * -
+     *
+     * @return string
+     */
     public function method(): string {
         return strtolower($_SERVER['REQUEST_METHOD']);
     }
 
+    /**
+     * -
+     *
+     * @return string
+     */
     public function uri(): string {
         $uri = $_SERVER['REQUEST_URI'];
 
         return explode('?', $uri)[0] ?? $uri;
     }
 
+    /**
+     * -
+     *
+     * @return array
+     */
     public function all(): array {
         return $_POST;
     }
 
-    public function get(string $key): string
+    /**
+     * -
+     *
+     * @param string $key
+     * @return mixed
+     */
+    #[Pure] public function get(string $key): mixed
     {
         return $this->all()[$key] ??= '';
     }
