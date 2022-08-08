80100959db9c98293f9af51bc334f7ce18d7a3e5
Khaled Mohsen <pres.kbayomy@gmail.com>
2022-08-08 22:35:46 +0200

    Improve [Session] class.

--- app/Session.php
+++ app/Session.php
@@ -6,72 +6,131 @@ use Minwork\Helper\Arr;
 
 class Session
 {
-    protected const FLASH_MESSAGES_KEY = 'flash_messages';
-
     public function __construct()
     {
         session_start();
 
-        $removableFlashMessages = array_filter($_SESSION[$this::FLASH_MESSAGES_KEY], fn(array $value) => !$value['remove']);
-
-        Arr::each(
-            $removableFlashMessages,
-            fn(string $key) => $_SESSION[$this::FLASH_MESSAGES_KEY][$key]['remove'] = true,
-            Arr::EACH_KEY_VALUE,
-        );
+        if (isset($_SESSION[FLASH_MESSAGES_KEY])) {
+            Arr::each(
+                $_SESSION[FLASH_MESSAGES_KEY],
+                fn(string $key) => $_SESSION[FLASH_MESSAGES_KEY][$key]['remove'] = true,
+                Arr::EACH_KEY_VALUE,
+            );
+        }
     }
 
-    public function set(string $key, mixed $value): void
+    /**
+     * -
+     *
+     * @param string $key
+     * @param mixed $value
+     * @param string|null $in
+     * @return void
+     */
+    public function set(string $key, mixed $value, string $in = null): void
     {
+        if ($in) {
+            $_SESSION[$in][$key] = $value;
+            return;
+        }
+
         $_SESSION[$key] = $value;
     }
 
-    public function has(string $key, bool $flash = false): bool
+    /**
+     * -
+     *
+     * @param string $keys
+     * @return bool
+     */
+    public function has(string $keys): bool
     {
-        if ($flash) {
-            return isset($_SESSION[$this::FLASH_MESSAGES_KEY][$key]);
-        }
-
-        return isset($_SESSION[$key]);
+        return Arr::has($_SESSION, $keys);
     }
 
-    public function get(string $key, bool $flash = false): mixed
+    /**
+     * -
+     *
+     * @param string $key
+     * @param string|null $from
+     * @return mixed
+     */
+    public function get(string $key, string $from = null): mixed
     {
-        if ($flash) {
-            return ($_SESSION[$this::FLASH_MESSAGES_KEY][$key]['content'] ??= false);
+        if ($from) {
+            return ($_SESSION[$from][$key] ??= false);
         }
 
         return ($_SESSION[$key] ??= false);
     }
 
-    public function flash(string $key, mixed $value): void
+    /**
+     * -
+     *
+     * @param string $key
+     * @param mixed $value
+     * @param string|null $in
+     * @return void
+     */
+    public function setFlash(string $key, mixed $value, string $in = null): void
     {
-        $_SESSION[$this::FLASH_MESSAGES_KEY][$key] = [
+        if ($in) {
+            $_SESSION[FLASH_MESSAGES_KEY][$in][$key] = $value;
+
+            return;
+        }
+
+        $_SESSION[FLASH_MESSAGES_KEY][$key] = [
             'remove' => false,
             'content' => $value,
         ];
     }
 
-    public function remove(string $key, bool $flash = false): void
+    /**
+     * -
+     *
+     * @param string $key
+     * @param string|null $from
+     * @return mixed
+     */
+    public function getFlash(string $key, string $from = null): mixed
     {
-        if ($flash) {
-            unset($_SESSION[$this::FLASH_MESSAGES_KEY][$key]);
+        if ($from) {
+            return $_SESSION[FLASH_MESSAGES_KEY][$from][$key] ??= false;
+        }
+
+        return $_SESSION[FLASH_MESSAGES_KEY][$key]['content'] ??= false;
+    }
+
+    /**
+     * -
+     *
+     * @param string $key
+     * @param string|null $from
+     * @return void
+     */
+    public function remove(string $key, string $from = null): void
+    {
+        if ($from) {
+            unset($_SESSION[FLASH_MESSAGES_KEY][$from][$key]);
             return;
         }
 
-        unset($_SESSION[$key]);
+        unset($_SESSION[FLASH_MESSAGES_KEY][$key]);
     }
 
     public function __destruct()
     {
-        Arr::each(
-            $_SESSION[$this::FLASH_MESSAGES_KEY],
-            function (string $key, array $value) {
-                if ($value['remove']) {
-                    $this->remove($key, true);
-                }
-            },
-            Arr::EACH_KEY_VALUE,
-        );
+        if (isset($_SESSION[FLASH_MESSAGES_KEY])) {
+            Arr::each(
+                $_SESSION[FLASH_MESSAGES_KEY],
+                function (string $key, array $value) {
+                    if (isset($value['remove']) && $value['remove']) {
+                        $this->remove($key);
+                    }
+                },
+                Arr::EACH_KEY_VALUE,
+            );
+        }
     }
 }
